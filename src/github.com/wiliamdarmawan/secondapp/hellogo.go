b5ea2a1370d5e4e79506c1b7df556e00e5ddcefc
wiliamdarmawan <generousbil@gmail.com>
2023-03-02 23:01:23 +0700

    Slice

--- src/github.com/wiliamdarmawan/secondapp/hellogo.go
+++ src/github.com/wiliamdarmawan/secondapp/hellogo.go
@@ -8,14 +8,36 @@ var pl = fmt.Println
 var pf = fmt.Printf
 
 func main() {
-	aStr1 := "abcde"
-	rArr := []rune(aStr1)
-	for _, v := range rArr{
-		pf("Rune Array: %d\n", v)
-
+	// var name []datatype
+	sl1 := make([]string, 6)
+	sl1[0] = "Society"
+	sl1[1] = "of"
+	sl1[2] = "the"
+	sl1[3] = "Spectacle"
+	sl1[4] = "by"
+	sl1[5] = "Guy Debord"
+	pl("Slice Size:", len(sl1))
+	for i := 0; i < len(sl1); i++ {
+		pl(sl1[i])
 	}
 
-	byteArr := []byte{'a', 'b', 'c' }
-	bStr := string(byteArr[:])
-	pl("I'm  a string: ", bStr)
+	for _, x := range sl1 {
+		pl(x)
+	}
+	sArr := [5]int { 1, 2, 3, 4, 5 }
+	sl2 := sArr[0:2]
+	pl("1st 3:", sArr[:3])
+	pl("Last 3: ", sArr[2:])
+	sArr[0] = 10
+	pl("sl2:", sl2)
+	sl2[0] = 1
+	pl("sArr:", sArr)
+
+	sl2 = append(sl2, 12)
+	pl("sl2:", sl2)
+	pl("sArr:", sArr)
+
+	sl3 := make([]string, 6)
+	pl("sl3:", sl3)
+	pl("sl3[0]:", sl3[0])
 }
--- src/github.com/wiliamdarmawan/secondapp/tempCodeRunnerFile.go
+++ src/github.com/wiliamdarmawan/secondapp/tempCodeRunnerFile.go
@@ -1,16 +1,2 @@
-package main
 
-import (
-	"fmt"
-	"math/rand"
-	"time"
-)
-
-var pl = fmt.Println
-var pf = fmt.Printf
-
-func main() {
-	seedSecs := time.Now().Unix()
-	randNum := rand.Seed(seedSecs)
-	pl("Random:", randNum)
-}
+		pl(x)
