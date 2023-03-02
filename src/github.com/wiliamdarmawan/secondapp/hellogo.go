58011e5541e49c66345c19313b91e0619df9dfd1
wiliamdarmawan <generousbil@gmail.com>
2023-03-02 22:14:16 +0700

    Data Types

--- src/github.com/wiliamdarmawan/secondapp/hellogo.go
+++ src/github.com/wiliamdarmawan/secondapp/hellogo.go
@@ -2,16 +2,29 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
-	"time"
 )
 
 var pl = fmt.Println
 var pf = fmt.Printf
 
 func main() {
-	seedSecs := time.Now().Unix()
-	rand.Seed(seedSecs)
-	randNum :=  rand.Intn(50) + 1
-	pl("Random:", randNum)
+	// %d : Integer
+	// %f : Float
+	// %s : String
+	// %t : Boolean
+	// %v : Value
+	// %T : Type
+	// %c : Character
+	// %o : Base 8
+	// %x : Base 16
+
+	pf("%s %d %c %f %t %o %x\n",
+		"Hello", 10, 'A', 3.14, true, 10, 10)
+
+	pf("%9f\n", 3.14)
+	pf("%.2f\n", 3.1412313)
+	pf("%9.f\n", 3.1441231)
+
+	sp1 := fmt.Sprintf("%9.f\n", 3.141592)
+	pl(sp1)
 }
