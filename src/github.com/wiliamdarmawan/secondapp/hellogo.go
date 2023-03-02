bb372ef0f00cd0b3fda3fe214a2c29e1661419b4
wiliamdarmawan <generousbil@gmail.com>
2023-03-02 22:08:50 +0700

    Random Time

--- src/github.com/wiliamdarmawan/secondapp/hellogo.go
+++ src/github.com/wiliamdarmawan/secondapp/hellogo.go
@@ -2,19 +2,16 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 var pl = fmt.Println
 var pf = fmt.Printf
 
 func main() {
-	pl("5 + 4", 5+4)
-	pl("5 - 4", 5-4)
-	pl("5 * 4", 5*4)
-	pl("5 / 4", 5/4)
-	pl("5 % 4", 5%4)
-	mInt := 1
-	mInt += 1
-	mInt++
-	pl(mInt)
+	seedSecs := time.Now().Unix()
+	rand.Seed(seedSecs)
+	randNum :=  rand.Intn(50) + 1
+	pl("Random:", randNum)
 }
