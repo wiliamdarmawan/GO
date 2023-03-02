a0a288bacd22f39fc6b12f970ad301820dc7921b
wiliamdarmawan <generousbil@gmail.com>
2023-03-02 23:25:27 +0700

    Pass array by value

--- src/github.com/wiliamdarmawan/secondapp/hellogo.go
+++ src/github.com/wiliamdarmawan/secondapp/hellogo.go
@@ -7,15 +7,16 @@ import (
 var pl = fmt.Println
 var pf = fmt.Printf
 
-func getSum2(nums ...int) int {
+func getArraySum(arr []int) int {
 	sum := 0
-	for _, num := range nums {
-		sum += num
+	for _, v := range arr {
+		sum += v
 	}
 	return sum
 }
 
 func main() {
 	// func funcName(parameters) returnType { BODY }
-	pl(getSum2(1, 2, 3, 4, 5))
+	vArr := []int{1, 2, 3, 4, 5}
+	pl("Array Sum:", getArraySum(vArr))
 }
