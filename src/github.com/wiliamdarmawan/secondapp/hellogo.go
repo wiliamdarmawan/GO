f4ec17200f256fc6effe42769deb93511ec10f20
wiliamdarmawan <generousbil@gmail.com>
2023-03-02 21:07:40 +0700

    Variables

--- src/github.com/wiliamdarmawan/secondapp/hellogo.go
+++ src/github.com/wiliamdarmawan/secondapp/hellogo.go
@@ -1,21 +1,20 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"log"
-	"os"
 )
 
 var pl = fmt.Println
 
 func main() {
-	pl("Hello World")
-	reader := bufio.NewReader(os.Stdin)
-	name, err := reader.ReadString('\n')
-	if err == nil {
-		pl("Hello", name)
-	} else {
-		log.Fatal(err)
-	}
-	pl(}
+	// var name type 
+	// if capital case, it's exported and able to be used by other packages
+	// if lower case, only in package scope only
+
+	var vName string = "Wiliam"
+	var v1, v2 = 1.2, 3.4
+	var v3 = "hello"
+	v4 := 5
+
+	pl(vName, v1, v2, v3, v4)
+}
