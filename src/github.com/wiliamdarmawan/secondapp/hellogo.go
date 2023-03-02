ad8a83f688c1191d58d39de89c32af51769ee743
wiliamdarmawan <generousbil@gmail.com>
2023-03-02 21:04:37 +0700

    Reader and error handler

--- src/github.com/wiliamdarmawan/secondapp/hellogo.go
+++ src/github.com/wiliamdarmawan/secondapp/hellogo.go
@@ -3,10 +3,19 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
 )
 
 var pl = fmt.Println
 
 func main() {
-	pl(
+	pl("Hello World")
+	reader := bufio.NewReader(os.Stdin)
+	name, err := reader.ReadString('\n')
+	if err == nil {
+		pl("Hello", name)
+	} else {
+		log.Fatal(err)
+	}
+	pl(}
