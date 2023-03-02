ae8e1eabdb1e4e80147c9d764a2399f4a969629a
wiliamdarmawan <generousbil@gmail.com>
2023-03-02 22:53:07 +0700

    Array

--- src/github.com/wiliamdarmawan/secondapp/hellogo.go
+++ src/github.com/wiliamdarmawan/secondapp/hellogo.go
@@ -8,27 +8,14 @@ var pl = fmt.Println
 var pf = fmt.Printf
 
 func main() {
-	var arr1 [5]int
-	arr1[0] = 1
-	arr2 := [5]int{1,2,3,4,5}
-	pl("Index 0:", arr2[0])
-	pl("Arr Length:", len(arr2))
+	aStr1 := "abcde"
+	rArr := []rune(aStr1)
+	for _, v := range rArr{
+		pf("Rune Array: %d\n", v)
 
-	for i :=0; i<len(arr2); i++ {
-		pl(arr2[i])
 	}
 
-	for i, v:= range arr2 {
-		pf("%d : %d\n", i, v)
-	}
-
-	arr3 := [2][2]int{
-		{1,2},
-		{3,4},
-	}
-	for i := 0; i <2; i++ {
-		for j := 0; j <2; j++ {
-			pf("%d ", arr3[i][j])
-		}		
-	}
+	byteArr := []byte{'a', 'b', 'c' }
+	bStr := string(byteArr[:])
+	pl("I'm  a string: ", bStr)
 }
