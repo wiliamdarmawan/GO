0acce975d44d1419100b6d70afc3006a93fb4a99
wiliamdarmawan <generousbil@gmail.com>
2023-03-02 22:01:49 +0700

    Runes

--- src/github.com/wiliamdarmawan/secondapp/hellogo.go
+++ src/github.com/wiliamdarmawan/secondapp/hellogo.go
@@ -2,31 +2,19 @@ package main
 
 import (
 	"fmt"
-	"strings"
+	"unicode/utf8"
 )
 
 var pl = fmt.Println
 var pf = fmt.Printf
 
 func main() {
-	sV1 := "A word"
-	// strings are just array of bytes ([]bytes)
-	
-	pl(sV1)
-	replacer := strings.NewReplacer("A", "Another", "word", "sentence")
-	sV2 := replacer.Replace(sV1)
-	pl(sV2)
+	// In GO, characters are called Runes
+	// Runes are unicodes that represent characters
+	rStr := "abcdefg"
+	pl("Rune Count:", utf8.RuneCountInString(rStr))
 
-	pl("Length:", len(sV2))
-	pl("Contains Another:", strings.Contains(sV2, "Another"))
-	pl("o index:", strings.Index(sV2, "o"))
-	pl("Replace:", strings.Replace(sV2, "o", "0", -1))
-
-	sV3 := "\nSome Words\n" // \t \" \\
-	sV3 = strings.TrimSpace(sV3)
-	pl("Split:", strings.Split("a-b-c-d", "-"))
-	pl("Lower:", strings.ToLower(sV3))
-	pl("Upper:", strings.ToUpper(sV3))
-	pl("Prefix:", strings.HasPrefix("tacocat", "taco"))
-	pl("Suffix:", strings.HasSuffix("tacocat", "cat"))
+	for i, runeVal := range rStr{
+		pf("%d : %#U : %c\n", i, runeVal, runeVal) // => %#U => Rune Unicode format
+	}
 }
