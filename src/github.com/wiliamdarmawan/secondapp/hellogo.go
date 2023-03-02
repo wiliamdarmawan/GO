ae308e9050a29d747b20b2859eb3a2810d6a623c
wiliamdarmawan <generousbil@gmail.com>
2023-03-02 23:23:58 +0700

    Unlimited parameter

--- src/github.com/wiliamdarmawan/secondapp/hellogo.go
+++ src/github.com/wiliamdarmawan/secondapp/hellogo.go
@@ -7,20 +7,15 @@ import (
 var pl = fmt.Println
 var pf = fmt.Printf
 
-func getQuotient(x float64, y float64) (ans float64, err error){
-	if y == 0 {
-		return 0, fmt.Errorf("Cannot divide by zero")
-	} else {
-		return x/y, nil
+func getSum2(nums ...int) int {
+	sum := 0
+	for _, num := range nums {
+		sum += num
 	}
+	return sum
 }
 
 func main() {
 	// func funcName(parameters) returnType { BODY }
-	quotient, err := getQuotient(10,2)
-	if err != nil {
-		pl(err)
-	} else {
-		pl(quotient)
-	}
+	pl(getSum2(1, 2, 3, 4, 5))
 }
