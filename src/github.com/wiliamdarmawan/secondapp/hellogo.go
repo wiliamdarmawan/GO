10deb1bae9273ca746a4f50c5538302db034f519
wiliamdarmawan <generousbil@gmail.com>
2023-03-02 21:44:38 +0700

    Strings

--- src/github.com/wiliamdarmawan/secondapp/hellogo.go
+++ src/github.com/wiliamdarmawan/secondapp/hellogo.go
@@ -2,21 +2,31 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 var pl = fmt.Println
 var pf = fmt.Printf
 
 func main() {
-	// Conditional Operators: ==, !=, <, >, <=, >=
-	// Logical Operators : && || !
+	sV1 := "A word"
+	// strings are just array of bytes ([]bytes)
+	
+	pl(sV1)
+	replacer := strings.NewReplacer("A", "Another", "word", "sentence")
+	sV2 := replacer.Replace(sV1)
+	pl(sV2)
 
-	iAge := 8
-	if iAge >= 18 {
-		pl("You can vote!")
-	} else {
-		pl("You can't vote!")
-	}
+	pl("Length:", len(sV2))
+	pl("Contains Another:", strings.Contains(sV2, "Another"))
+	pl("o index:", strings.Index(sV2, "o"))
+	pl("Replace:", strings.Replace(sV2, "o", "0", -1))
 
-	pl("!true =", !true)
+	sV3 := "\nSome Words\n" // \t \" \\
+	sV3 = strings.TrimSpace(sV3)
+	pl("Split:", strings.Split("a-b-c-d", "-"))
+	pl("Lower:", strings.ToLower(sV3))
+	pl("Upper:", strings.ToUpper(sV3))
+	pl("Prefix:", strings.HasPrefix("tacocat", "taco"))
+	pl("Suffix:", strings.HasSuffix("tacocat", "cat"))
 }
