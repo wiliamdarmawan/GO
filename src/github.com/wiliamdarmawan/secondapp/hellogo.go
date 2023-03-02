a745683ecd7dcd1e41e5123674fedf632bf01093
wiliamdarmawan <generousbil@gmail.com>
2023-03-02 23:49:29 +0700

    File IO 2

--- src/github.com/wiliamdarmawan/secondapp/hellogo.go
+++ src/github.com/wiliamdarmawan/secondapp/hellogo.go
@@ -1,50 +1,46 @@
 package main
 
 import (
-	"bufio"
+	"errors"
 	"fmt"
 	"log"
 	"os"
-	"strconv"
 )
 
 var pl = fmt.Println
 var pf = fmt.Printf
 
-func changeVal2(myPtr *int) {
-	*myPtr = 12
-}
-
 func main() {
-	// func funcName(parameters) returnType { BODY }
-	f, err := os.Create("data.txt")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer f.Close()
+	/*
+		Exactly one of O_RDONLY, O_WRONLY, or O_RDWR must be specified.
 
-	iPrimeArr := []int{2, 3, 5, 7, 11}
-	var sPrimeArr []string
-	for _, i := range iPrimeArr {
-		sPrimeArr = append(sPrimeArr, strconv.Itoa(i))
-	}
-	for _, num := range sPrimeArr {
-		_, err := f.WriteString(num + "\n")
+		O_RDONLY : open the file read-only.
+		O_WRONLY : open the file write-only.
+		O_RDWR : open the file read-write.
+
+		These can be or'ed
+
+		O_APPEND : append data to the file when writing.
+		O_CREATE : create a new file if none exists.
+		O_EXCL : used with O_CREATE, file must not exist.
+		O_SYNC : open for synchronous I/O.
+		O_TRUNC : if possible, truncate file when opened.
+	*/
+
+	_, err := os.Stat("data.txt")
+	if errors.Is(err, os.ErrNotExist) {
+		pl("File doesn't exist")
+	} else {
+		f, err := os.OpenFile("data.txt", os.O_APPEND | os.O_CREATE | os.O_WRONLY, 0644)
 		if err != nil {
 			log.Fatal(err)
 		}
-	}
+		defer f.Close()
 
-	f, err = os.Open("data.txt")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer f.Close()
-	scan1 := bufio.NewScanner(f)
-	for scan1.Scan() {
-		pl("Prime:", scan1.Text())
-	}
-	if err := scan1.Err(); err != nil {
-		log.Fatal(err)
+		if _, err := f.WriteString("13\n"); err != nil {
+			log.Fatal(err)
+		}
+
+		
 	}
 }
