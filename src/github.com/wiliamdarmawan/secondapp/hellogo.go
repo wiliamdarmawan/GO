da1891e53c5555d9d3d2c760579df035c15a06b0
wiliamdarmawan <generousbil@gmail.com>
2023-03-02 22:16:30 +0700

    For Loop

--- src/github.com/wiliamdarmawan/secondapp/hellogo.go
+++ src/github.com/wiliamdarmawan/secondapp/hellogo.go
@@ -8,23 +8,16 @@ var pl = fmt.Println
 var pf = fmt.Printf
 
 func main() {
-	// %d : Integer
-	// %f : Float
-	// %s : String
-	// %t : Boolean
-	// %v : Value
-	// %T : Type
-	// %c : Character
-	// %o : Base 8
-	// %x : Base 16
+	// for initialization; condition;
+	// postStatement { BODY }
 
-	pf("%s %d %c %f %t %o %x\n",
-		"Hello", 10, 'A', 3.14, true, 10, 10)
+	for x := 1; x <=5; x++{
+		pl(x)
+	}
 
-	pf("%9f\n", 3.14)
-	pf("%.2f\n", 3.1412313)
-	pf("%9.f\n", 3.1441231)
-
-	sp1 := fmt.Sprintf("%9.f\n", 3.141592)
-	pl(sp1)
+	fX := 0
+	for fX <= 5 {
+		pl(fX)
+		fX++
+	}
 }
