c354874f2f990e4110ebb427ff7dd37bba543d25
wiliamdarmawan <generousbil@gmail.com>
2023-03-02 23:55:38 +0700

    Command Lines

---
+++ src/github.com/wiliamdarmawan/secondapp/cltest.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main(){
+	fmt.Println(os.Args)
+	args := os.Args[1:]
+	var iArgs = []int{}
+	for _, i := range args{
+		val, err := strconv.Atoi(i)
+		if err != nil {
+			panic(err)
+		}
+		iArgs = append(iArgs, val)
+	}
+
+	max := 0
+	for _ , val := range iArgs {
+		if val > max {
+			max = val
+		}
+	}
+
+	fmt.Println("Max Value:", max)
+}
