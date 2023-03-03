1aff8b9ef4df913483d2bdc1ff059728f70bbc75
wiliamdarmawan <generousbil@gmail.com>
2023-03-04 03:15:07 +0700

    Regex

--- src/github.com/wiliamdarmawan/secondapp/hellogo.go
+++ src/github.com/wiliamdarmawan/secondapp/hellogo.go
@@ -2,18 +2,24 @@ package main
 
 import (
 	"fmt"
+	"regexp"
 )
 
 var pl = fmt.Println
 var pf = fmt.Printf
 
-func factorial(num int) int {
-	if num == 0 {
-		return 1
-	}
-	return num * factorial(num-1)
-}
-
 func main() {
-	pl("Factorial 4 =", factorial(4))
+	reStr := "The ape was at the apex"
+	match, _ := regexp.MatchString("(ape[^ ]?", reStr)
+	pl(match)
+
+	reStr2 := "Cat rat mat fat pat"
+	r, _ := regexp.Compile("([crmfp]at)")
+	pl("MatchString:", r.MatchString(reStr2))
+	pl("FindString:", r.FindString(reStr2))
+	pl("FindStringIndex:", r.FindStringIndex(reStr2))
+	pl("All String:", r.FindAllString(reStr2, -1))
+	pl("1st 2 Strings:", r.FindAllString(reStr2, 2))
+	pl("All Submatch Index:", r.FindAllStringSubmatchIndex(reStr2, -1))
+	pl(r.ReplaceAllString(reStr2, "Dog"))
 }
