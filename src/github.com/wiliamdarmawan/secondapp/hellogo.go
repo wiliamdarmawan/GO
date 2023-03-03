f2228567f4ce55899733cfce07221b49a74bf1a8
wiliamdarmawan <generousbil@gmail.com>
2023-03-03 20:53:49 +0700

    Map

--- src/github.com/wiliamdarmawan/secondapp/hellogo.go
+++ src/github.com/wiliamdarmawan/secondapp/hellogo.go
@@ -1,46 +1,36 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"log"
-	"os"
 )
 
 var pl = fmt.Println
 var pf = fmt.Printf
 
 func main() {
-	/*
-		Exactly one of O_RDONLY, O_WRONLY, or O_RDWR must be specified.
+	// var myMap map [keyType]valueType
+	var heroes map[string]string
+	heroes = make(map[string]string)
+	villains := make(map[string]string)
+	heroes["Batman"] = "Bruce Wayne"
+	heroes["Superman"] = "Clark Kent"
+	heroes["Flash"] = "Barry Allen"
+	villains["Lex Luthor"] = "Lex Luthor"
 
-		O_RDONLY : open the file read-only.
-		O_WRONLY : open the file write-only.
-		O_RDWR : open the file read-write.
-
-		These can be or'ed
-
-		O_APPEND : append data to the file when writing.
-		O_CREATE : create a new file if none exists.
-		O_EXCL : used with O_CREATE, file must not exist.
-		O_SYNC : open for synchronous I/O.
-		O_TRUNC : if possible, truncate file when opened.
-	*/
-
-	_, err := os.Stat("data.txt")
-	if errors.Is(err, os.ErrNotExist) {
-		pl("File doesn't exist")
-	} else {
-		f, err := os.OpenFile("data.txt", os.O_APPEND | os.O_CREATE | os.O_WRONLY, 0644)
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer f.Close()
-
-		if _, err := f.WriteString("13\n"); err != nil {
-			log.Fatal(err)
-		}
-
-		
+	superPets := map[int]string {
+		1: "Krypto",
+		2: "Bat Hound",
 	}
+
+	pf("Batman is %v\n", heroes["Batman"])
+	pl("Chip:", superPets[3])
+
+	_, ok := superPets[3]
+	 pl("Is there a 3rd pet:", ok)
+
+	 for k, v := range heroes {
+		pf("%s is %s\n", k, v)
+	 }
+	 delete(heroes, "Flash")
+	 pl(heroes)
 }
