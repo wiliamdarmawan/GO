ec138ab5d3e86dee9d99d0f61fda7c58db3cb8ae
wiliamdarmawan <generousbil@gmail.com>
2023-03-04 02:58:50 +0700

    Mutex lock

--- src/github.com/wiliamdarmawan/secondapp/hellogo.go
+++ src/github.com/wiliamdarmawan/secondapp/hellogo.go
@@ -2,31 +2,42 @@ package main
 
 import (
 	"fmt"
+	"sync"
+	"time"
 )
 
 var pl = fmt.Println
 var pf = fmt.Printf
 
-func nums1(channel chan int){
-	channel <- 1
-	channel <- 2
-	channel <- 3
+type Account struct{
+	balance int
+	lock sync.Mutex
 }
 
-func nums2(channel chan int){
-	channel <- 4
-	channel <- 5
-	channel <- 6
+func (a *Account) GetBalance() int {
+	a.lock.Lock()
+	defer a.lock.Unlock()
+	return a.balance
+}
+
+func (a *Account) Withdraw(v int) {
+	a.lock.Lock()
+	defer a.lock.Unlock()
+
+	if v > a.balance {
+		pl("Not enough money in account")
+	} else {
+		pf("%d withdrawn: Balance : %d\n", v, a.balance)
+		a.balance -= v
+	}
 }
 
 func main() {
-	channel1 := make(chan int)
-	channel2 := make(chan int)
-
-	go nums1(channel1)
-	go nums2(channel2)
-	pl(<-channel1)
-	pl(<-channel2)
-	pl(<-channel1)
-	pl(<-channel2)
+	var acct Account
+	acct.balance = 100
+	pl("Balance : ", acct.GetBalance())
+	for i := 0; i < 12; i++ {
+		go acct.Withdraw(10)
+	}
+	time.Sleep(2 * time.Second)
 }
