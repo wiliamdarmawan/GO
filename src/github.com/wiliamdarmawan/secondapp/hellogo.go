a5c12044800eaf0e4fdf101b735aa08d882994be
wiliamdarmawan <generousbil@gmail.com>
2023-03-03 21:36:11 +0700

    Composition

--- src/github.com/wiliamdarmawan/secondapp/hellogo.go
+++ src/github.com/wiliamdarmawan/secondapp/hellogo.go
@@ -7,15 +7,33 @@ import (
 var pl = fmt.Println
 var pf = fmt.Printf
 
-type rectangle struct {
-	length, height float64
+type contact struct {
+	fName string
+	lName string
+	phone string
 }
 
-func (r rectangle) Area() float64 {
-	return r.length * r.height
+type business struct {
+	name string
+	address string
+	contact
+}
+
+func (b business) info() {
+	pf("Contact at %s is %s %s", b.name, 
+	b.contact.fName, b.contact.lName)
 }
 
 func main() {
-	rect1 := rectangle{ 10.0, 15.0 }
-	pl("Rect Area:", rect1.Area())
+	con1 := contact { 
+		fName: "Wiliam", 
+		lName: "Darmawan", 
+		phone: "1234567890",
+	}
+	biz1 := business { 
+		name: "Wiliam's Business", 
+		address: "123 Main St", 
+		contact: con1,
+	}
+	biz1.info()
 }
