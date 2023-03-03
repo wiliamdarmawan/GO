27517ed5198b23ef1c76578154a7f0c9f604f068
wiliamdarmawan <generousbil@gmail.com>
2023-03-04 01:51:37 +0700

    Go Routines + Sleep

--- src/github.com/wiliamdarmawan/secondapp/hellogo.go
+++ src/github.com/wiliamdarmawan/secondapp/hellogo.go
@@ -2,40 +2,26 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 var pl = fmt.Println
 var pf = fmt.Printf
 
-type Animal interface {
-	AngrySound()
-	HappySound()
+func printTo15() {
+	for i := 1; i <= 15; i++ {
+		pl("Fun 1:", i)
+	}
 }
 
-type Cat string
-
-func (c Cat) Attack() {
-	pl("cat Attacks its Prey")
-}
-
-func (c Cat) Name() string{
-	return string(c)
-}
-
-func (c Cat) AngrySound() {
-	pl("Cat says Hissss")
-}
-
-func (c Cat) HappySound() {
-	pl("Cat says Purrrr")
+func printTo10() {
+	for i := 1; i <= 10; i++ {
+		pl("Fun 2:", i)
+	}
 }
 
 func main() {
-	var kitty Animal
-	kitty = Cat("Kitty")
-	kitty.AngrySound()
-	
-	var kitty2 Cat = kitty.(Cat)
-	kitty2.Attack()
-	pl("Cats Name:", kitty2.Name())
+	go printTo10()
+	go printTo15()
+	time.Sleep(2 * time.Second)
 }
