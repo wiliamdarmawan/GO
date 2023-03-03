a90a2e1c830d82503e4e1c35a703e96ed44a0fac
wiliamdarmawan <generousbil@gmail.com>
2023-03-04 02:52:53 +0700

    Channels

--- src/github.com/wiliamdarmawan/secondapp/hellogo.go
+++ src/github.com/wiliamdarmawan/secondapp/hellogo.go
@@ -2,26 +2,31 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 var pl = fmt.Println
 var pf = fmt.Printf
 
-func printTo15() {
-	for i := 1; i <= 15; i++ {
-		pl("Fun 1:", i)
-	}
+func nums1(channel chan int){
+	channel <- 1
+	channel <- 2
+	channel <- 3
 }
 
-func printTo10() {
-	for i := 1; i <= 10; i++ {
-		pl("Fun 2:", i)
-	}
+func nums2(channel chan int){
+	channel <- 4
+	channel <- 5
+	channel <- 6
 }
 
 func main() {
-	go printTo10()
-	go printTo15()
-	time.Sleep(2 * time.Second)
+	channel1 := make(chan int)
+	channel2 := make(chan int)
+
+	go nums1(channel1)
+	go nums2(channel2)
+	pl(<-channel1)
+	pl(<-channel2)
+	pl(<-channel1)
+	pl(<-channel2)
 }
