ba3057c4be836f499bfd3d163224a5398aa83348
wiliamdarmawan <generousbil@gmail.com>
2023-03-03 21:28:27 +0700

    Struct 1

--- src/github.com/wiliamdarmawan/secondapp/hellogo.go
+++ src/github.com/wiliamdarmawan/secondapp/hellogo.go
@@ -7,17 +7,29 @@ import (
 var pl = fmt.Println
 var pf = fmt.Printf
 
-type MyConstraint interface {
-	int | float64
+type customer struct {
+	name string
+	address string
+	bal float64
 }
 
-// pkg.go.dev/golang.org/x/exp/constraints
+func getCustInfo(c customer) {
+	pf("%s owes us %.2f\n", c.name, c.bal)
+}
 
-func getSumGen[T MyConstraint](x T, y T) T {
-	return x + y
+func newCustAdd(c *customer, address string) {
+	c.address = address
 }
 
 func main() {
-	pl("5 + 4 =", getSumGen(5, 4))
-	pl("5.6 + 4.7 =", getSumGen(5.6, 4.7))
+	 var tS customer
+	 tS.name = "Wiliam Darmawan"
+	 tS.address = "Jl. Kebon Jeruk Raya No. 1"
+	 tS.bal = 234.56
+	 getCustInfo(tS)
+	 newCustAdd(&tS, "123 South st")
+	 pl("Address:", tS.address)
+	 sS := customer{"Sally Smith", "123 South st", 0.0}
+
+	 pl("Name:", sS.name)
 }
