d06662b4e38fcffa82be62d0171424b48892c41c
wiliamdarmawan <generousbil@gmail.com>
2023-03-04 03:07:08 +0700

    Recursion

--- src/github.com/wiliamdarmawan/secondapp/hellogo.go
+++ src/github.com/wiliamdarmawan/secondapp/hellogo.go
@@ -7,27 +7,13 @@ import (
 var pl = fmt.Println
 var pf = fmt.Printf
 
-func useFunction(f func(int, int) int, x, y int) {
-	pl("Answer:", f(x, y))
-}
-
-func sumValues(x, y int) int {
-	return x + y
+func factorial(num int) int {
+	if num == 0 {
+		return 1
+	}
+	return num * factorial(num-1)
 }
 
 func main() {
-	intSum := func(x, y int) int {
-		return x + y
-	}
-	pl("5 + 4 =", intSum(5, 4))
-
-	samp1 := 1
-	changeVar := func() {
-		samp1 += 1
-	}
-
-	changeVar()
-	pl("samp1 =", samp1)
-
-	useFunction(sumValues, 5, 8)
+	pl("Factorial 4 =", factorial(4))
 }
