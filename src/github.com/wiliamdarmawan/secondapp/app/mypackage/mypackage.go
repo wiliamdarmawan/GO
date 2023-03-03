a0edfe6d4398e020454228f32f9378ce76021430
wiliamdarmawan <generousbil@gmail.com>
2023-03-04 01:23:36 +0700

    Getter Setter

--- src/github.com/wiliamdarmawan/secondapp/app/mypackage/mypackage.go
+++ src/github.com/wiliamdarmawan/secondapp/app/mypackage/mypackage.go
@@ -1,7 +1,9 @@
 package stuff
 
 import (
+	"errors"
 	"strconv"
+	"time"
 )
 
 var Name string = "Wiliam Darmawan"
@@ -12,4 +14,49 @@ var Name string = "Wiliam Darmawan"
 		strArr = append(strArr, strconv.Itoa(i))
 	}
 	return strArr
+ }
+
+ type Date struct {
+	day int
+	month int
+	year int
+ }
+
+ func (d *Date) SetDay(day int) error{
+	if day < 1 || day > 31 {
+		return errors.New("incorrect day value")
+	} else {
+		d.day = day
+		return nil
+	}
+ }
+
+ func (d *Date) SetMonth(month int) error{
+	if month < 1 || month > 12 {
+		return errors.New("incorrect month value")
+	} else {
+		d.month = month
+		return nil
+	}
+ }
+
+ func (d *Date) SetYear(year int) error{
+	if year < 1875 || year > time.Now().Year() {
+		return errors.New("incorrect year value")
+	} else {
+		d.year = year
+		return nil
+	}
+ }
+
+ func (d *Date) Day() int {
+	return d.day
+ }
+
+ func (d *Date) Month() int {
+	return d.month
+ }
+
+ func (d *Date) Year() int {
+	return d.year
  }
