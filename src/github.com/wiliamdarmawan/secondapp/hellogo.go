e8e9194e9d157fe68b166bb2b2be4726c9c78a0f
wiliamdarmawan <generousbil@gmail.com>
2023-03-03 21:22:37 +0700

    Generics

--- src/github.com/wiliamdarmawan/secondapp/hellogo.go
+++ src/github.com/wiliamdarmawan/secondapp/hellogo.go
@@ -7,30 +7,17 @@ import (
 var pl = fmt.Println
 var pf = fmt.Printf
 
+type MyConstraint interface {
+	int | float64
+}
+
+// pkg.go.dev/golang.org/x/exp/constraints
+
+func getSumGen[T MyConstraint](x T, y T) T {
+	return x + y
+}
+
 func main() {
-	// var myMap map [keyType]valueType
-	var heroes map[string]string
-	heroes = make(map[string]string)
-	villains := make(map[string]string)
-	heroes["Batman"] = "Bruce Wayne"
-	heroes["Superman"] = "Clark Kent"
-	heroes["Flash"] = "Barry Allen"
-	villains["Lex Luthor"] = "Lex Luthor"
-
-	superPets := map[int]string {
-		1: "Krypto",
-		2: "Bat Hound",
-	}
-
-	pf("Batman is %v\n", heroes["Batman"])
-	pl("Chip:", superPets[3])
-
-	_, ok := superPets[3]
-	 pl("Is there a 3rd pet:", ok)
-
-	 for k, v := range heroes {
-		pf("%s is %s\n", k, v)
-	 }
-	 delete(heroes, "Flash")
-	 pl(heroes)
+	pl("5 + 4 =", getSumGen(5, 4))
+	pl("5.6 + 4.7 =", getSumGen(5.6, 4.7))
 }
