b64e6d676910d680a09712e818919c8e38febcf4
wiliamdarmawan <generousbil@gmail.com>
2023-03-04 01:23:45 +0700

    Encapsulation

--- src/github.com/wiliamdarmawan/secondapp/app/main.go
+++ src/github.com/wiliamdarmawan/secondapp/app/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	stuff "example/project/mypackage"
 	"fmt"
+	"log"
 	"reflect"
 )
 
@@ -12,4 +13,22 @@ func main(){
 	strArr := stuff.IntArrToStrArray(intArr)
 	fmt.Println(strArr)
 	fmt.Println(reflect.TypeOf(strArr))
+
+	date := stuff.Date{}
+	err := date.SetMonth(12)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = date.SetDay(21)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = date.SetYear(1974)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("1st Day : %d/%d/%d\n", date.Day(), date.Month(), date.Year())
 }
